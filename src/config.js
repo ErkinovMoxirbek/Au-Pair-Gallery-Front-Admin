37dd774ce3306f1f57b4fbb5f9e9be42d332d08f
ASUS TUF <erkinov.0021@gmail.com>
2025-12-13 16:15:03 +0500

    fixed

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:8080/api/v1";
+const API_BASE_URL = "https://api.au-pair-gallery.de/api/v1";
 export default API_BASE_URL;
 
 // au-pair-gallery-backend.onrender.com
